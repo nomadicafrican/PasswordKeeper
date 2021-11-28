610a149acf35331455992d34fd1828b099a0daf8
Farida <faridamus@protonmail.com>
2021-11-28 20:47:37 +0000

    Revise database after a group meeting

--- db/schema/03_users_organizations.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS users_organizations CASCADE;
-
-CREATE TABLE users_organizations (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  organization_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE
-);
