8872dc0ed00baa42569fd2544e764643f93255af
Farida <faridamus@protonmail.com>
2021-11-29 17:14:56 +0000

    Fix the users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,11 +1,11 @@
  INSERT INTO users
- VALUES (1,'richard@gmail.com', 'Richard','123456'),
- (2,'lence@gmail.com','Lence', '0909090'),
- (3,'osmo@gmail.com', 'Osmo','131313'),
- (4,'tommy@gmail.com','Tommy','14567'),
- (5,'lanny@gmail.com','lanny','98765'),
- (6,'emma@gmail.com','emma', '88882222'),
- (7,'peter@gmail.com','peter', '049293929'),
- (8,'elvie@gmail.com','elvie', '4444444'),
- (9,'alvin@gmail.com', 'alvin', '55334223'),
- (10,'lindsay@gmail.com','lindsay', '54345345');
+ VALUES (1,'richard@gmail.com', 'Richard','123456', 1),
+ (2,'lence@gmail.com','Lence', '0909090', 2),
+ (3,'osmo@gmail.com', 'Osmo','131313', 3),
+ (4,'tommy@gmail.com','Tommy','14567', 2),
+ (5,'lanny@gmail.com','lanny','98765', 1),
+ (6,'emma@gmail.com','emma', '88882222', 2),
+ (7,'peter@gmail.com','peter', '049293929', 1),
+ (8,'elvie@gmail.com','elvie', '4444444', 1),
+ (9,'alvin@gmail.com', 'alvin', '55334223', 2),
+ (10,'lindsay@gmail.com','lindsay', '54345345', 3);
