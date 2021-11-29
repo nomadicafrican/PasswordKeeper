5732903872297b2318bc9311e7c2c60c688a8014
Ammar <ammar.kusow2@gmail.com>
2021-11-29 12:13:05 -0500

    fixing seeds

--- db/seeds/02_accounts_seeds.sql
+++ db/seeds/02_accounts_seeds.sql
@@ -1,19 +1,19 @@
 INSERT INTO accounts
 VALUES (1, 1, 'facebook', 'LHL@facebook.com', 'password' , 'entertainment' );
 INSERT INTO accounts
-VALUES (2, 1, 'coding', 'LHL@coding.com', 'password' , 'coding' );
+VALUES (2, 1, 'webacademy', 'LHL@webacademy.com', 'password' , 'coding' );
 INSERT INTO accounts
 VALUES (3, 1, 'youtube', 'LHL@youtube.com', 'password' , 'entertainment' );
 INSERT INTO accounts
 VALUES (4, 2, 'facebook', 'amazon@facebook.com', 'password' , 'entertainment' );
 INSERT INTO accounts
-VALUES (5, 2, 'coding', 'amazon@coding.com', 'password' , 'coding' );
+VALUES (5, 2, 'webacademy', 'amazon@webacademy.com', 'password' , 'coding' );
 INSERT INTO accounts
 VALUES (6, 2, 'youtube', 'amazon@youtube.com', 'password' , 'entertainment' );
 INSERT INTO accounts
 VALUES (7, 3, 'facebook', 'facebook@facebook.com', 'password' , 'entertainment' );
 INSERT INTO accounts
-VALUES (8, 3, 'facebook', 'facebook@coding.com', 'password' , 'coding' );
+VALUES (8, 3, 'webacademy', 'facebook@webacademy.com', 'password' , 'coding' );
 INSERT INTO accounts
 VALUES (9, 3, 'facebook', 'facebook@youtube.com', 'password' , 'entertainment' );
 
