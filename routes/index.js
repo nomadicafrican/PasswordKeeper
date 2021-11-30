abaea91599220e8db291c43699528524f877dc55
Farida <faridamus@protonmail.com>
2021-11-30 17:53:01 +0000

    Add bootstrap markup to welcome.ejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 //GET request
-router.get('/', (req, res) => res.send('Welcome'))
+router.get('/', (req, res) => res.render('Welcome'))
 
 module.exports = router;
 
