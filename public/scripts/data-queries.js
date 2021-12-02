7ba51f335e2560638e910d9e26776065160dd5a2
Justin Tu <coppolavina@gmail.com>
2021-12-02 02:57:01 +0000

    after login, show users a list of their organization username and password

--- public/scripts/data-queries.js
+++ public/scripts/data-queries.js
@@ -1,10 +1,19 @@
 
 const getEmailUserPass = (org_id,db) => {
-  return db.query(`SELECT name, organizations_email, organizations_password FROM accounts WHERE organization_id =$1`,[org_id])
+  return db.query(`SELECT name, organizations_email, organizations_password, category FROM accounts WHERE organization_id =$1`,[org_id])
     .then((result) => {
       return result.rows;
     });
 };
+
+// const deletePassword = (org_id,acc_id, db) => {
+//   return db.query(`UPDATE accounts SET password = NULL
+//   WHERE organization_id =$1 AND accounts.id = $2;`, [ org_id, acc_id ])
+//   .then((result) => {
+//     // console.log(result.rows)
+//     return result.rows;
+//   });
+// }
 //
 module.exports = {getEmailUserPass};
 
--- routes/getUserAndPass.js
+++ routes/getUserAndPass.js
@@ -7,7 +7,42 @@ module.exports = (db) => {
   router.get('/:id',(req,res) => {
     GetdataFunc.getEmailUserPass(req.params.id,db)
       .then((data) => {
-        res.json(data)
+        const arr =[];
+        const templateVar = {};
+
+        for (let i in data) {
+          arr.push(data[i].name)
+        }
+        console.log(arr)
+
+
+        for (let j in data) {
+          for (let l of arr) {
+            if (data[j].name === l) {
+              templateVar[l] = data[j]
+              }
+            }
+          }
+
+        const result = {templateVar}
+        // // const orgName = data[0].name;
+        // // const SiteUsername = [];
+        // // const OrgPass= [];
+        // // const Cate= [];
+        // // for (let i in data) {
+        // //   SiteUsername.push(data[i].organizations_email);
+        // //   OrgPass.push(data[i].organizations_password);
+        // //   Cate.push(data[i].category)
+        // // }
+
+        // // templateVar = {
+        // //   orgName,
+        // //   SiteUsername,
+        // //   OrgPass,
+        // //   Cate
+        // // }
+        // console.log(templateVar)
+        res.render("loadingID", result)
       })
       .catch(err => {
         res
@@ -15,8 +50,27 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // router.post('/:id/delete',(req,res) => {
+  //   GetdataFunc.deletePassword(req.params.id,db)
+  //     .then((data) => {
+  //       res.json(data)
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  // });
+
+
+
+
   return router;
 };
+
+
+
 //
 
 
